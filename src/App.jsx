ea7c2da7408495ae952a589a4c21ebcf07e731ba
JAZMIN ARIELA BEIGEL <47873291@ort>
2025-04-30 09:40:05 -0300

    Cambios completos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //import { useState } from 'react'
 import './App.css'
 import Citas from './componentes/Citas'
-import Forms from './componentes/form'
+import Forms from './componentes/Forms'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -14,7 +14,7 @@ function App() {
         <div class="row">
           <div class="one-half column">
             <h2>Crear mi Cita</h2>
-            <Forms></Forms>
+            <Forms />
           </div>
           <div class="one-half column">
             <h2>Administra tus citas</h2>
